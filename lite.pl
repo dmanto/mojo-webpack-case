1891a3878b82c025b8528e29c00fbcc3bfc03467
Daniel Mantovani <dmanto@gmail.com>
2021-02-26 16:49:30 -0300

    sample helper for images case

--- lite.pl
+++ lite.pl
@@ -1,10 +1,27 @@
 #!/usr/bin/env perl
-use Mojolicious::Lite;
+use Mojolicious::Lite -signatures;
+use Mojo::JSON 'decode_json';
 use FindBin;
 BEGIN { unshift @INC, "$FindBin::Bin/../../lib" }
 
+# this is a just a hack to get the stats json file
+$ENV{MOJO_WEBPACK_BUILD} = '--json=mojo-wp.json';
 plugin Webpack => {process => [qw(js css sass)]};
-get '/'        => 'index';
+
+# this helper will return the new url after webpack compilation
+# it reads webpack's stats file, parses it and then referes the
+# result to the public directory
+app->helper(
+  wp_url => sub ($c, $desc) {
+    state $wp_info = decode_json $c->app->home->child('mojo-wp.json')->slurp;
+    state %assets  = map { $_->{info}{sourceFilename} => $_->{name} }
+      grep { exists $_->{info}{sourceFilename} } @{$wp_info->{assets}};
+
+    # $c->app->log->warn ($c->app->dumper($wp_info->{assets}));
+    return '/asset/' . $assets{$desc};
+  }
+);
+get '/' => 'index';
 app->start;
 
 __DATA__
@@ -22,7 +39,7 @@ __DATA__
       This is a demo for using <a href="https://webpack.js.org/">Webpack</a>
       together with <a href="https://mojolicious.org/">Mojolicious</a>.
     </p>
-    %= image 'asset/wave.jpg'
+    %= image wp_url 'assets/images/wave.jpg'
     %#= image 'asset/wave-faad2aefeace0ad5b207e8c98530a001.jpg'
   </body>
 </html>
