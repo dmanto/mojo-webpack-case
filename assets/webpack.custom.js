2616f714782645ea1530cf96e336ff598c70b1b6
Daniel Mantovani <dmanto@gmail.com>
2021-02-26 12:24:01 -0300

    copy image to public/asset dir

--- assets/webpack.custom.js
+++ assets/webpack.custom.js
@@ -1,5 +1,10 @@
-module.exports = function(config) {
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = function (config) {
   config.entry = {
     'example': './assets/js/app.js',
   };
+  config.plugins.push(
+    new CopyPlugin({ patterns: [{ from: "assets/images" }] })
+  );
 };
